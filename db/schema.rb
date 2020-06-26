cabdc5c9f7748a55dabd9835f3d7b6e8e861d544
juanmme <juanmmescobar@gmail.com>
2020-06-27 00:38:14 +0200

    feat: add one to many association between recipe and chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_210704) do
+ActiveRecord::Schema.define(version: 2020_06_26_221540) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_26_210704) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "chef_id"
   end
 
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
     def setup
-        @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe")
+        @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
+        @recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable recipe")
+
+        # Alternatives:
+        # @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe", chef: @chef)
+        # @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe", chef_id: @chef.id)
+    end
+
+    test "recipe without chef should be invalid" do
+        @recipe.chef_id = nil
+        assert_not @recipe.valid?
     end
 
     test "recipe should be valid" do
