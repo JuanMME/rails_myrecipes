cfcfa582ed526d2aac0b3e060547ebadffff0120
juanmme <juanmmescobar@gmail.com>
2020-07-02 13:03:42 +0200

    feat: add ingredient model and many-to-many association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ApplicationRecord
 
     belongs_to :chef
     validates :chef_id, presence: true
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
 
     default_scope -> { order(updated_at: :desc) }
 end
