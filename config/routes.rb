61bc829474fa28cb2299a73cf436cb730f869cff
juanmme <juanmmescobar@gmail.com>
2020-07-03 08:49:48 +0200

    feat(ingredients): implement CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
     # get 'recipes/new', to: "recipes#new", as: "new_recipe"
     # get "recipes/:id", to: "recipes#show", as: "recipe"
     # post "recipes", to: "recipes#create"
-    resources :recipes
     get "/signup", to: "chefs#new"
+    resources :recipes
     resources :chefs, except: [:new]
+    resources :ingredients, except: [:destroy]
 
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
